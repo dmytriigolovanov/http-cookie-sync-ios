d18a42dbb476fc73cbcd9411f1068055fe9dbe30
Dmytrii Golovanov <dmytrii.golovanov@gmail.com>
2022-06-10 20:05:35 +0300

    Setup xcodeproj

---
+++ HTTPCookieSync/Sources/Headers/HTTPCookieSync.h
@@ -0,0 +1,18 @@
+//
+//  HTTPCookieSync.h
+//  HTTPCookieSync
+//
+//  Created by Dmytrii Golovanov on 10.06.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HTTPCookieSync.
+FOUNDATION_EXPORT double HTTPCookieSyncVersionNumber;
+
+//! Project version string for HTTPCookieSync.
+FOUNDATION_EXPORT const unsigned char HTTPCookieSyncVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTPCookieSync/PublicHeader.h>
+
+
