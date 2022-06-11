8fe69ebffcb60aa0292d47701466e6a89810feb5
Dmytrii Golovanov <dmytrii.golovanov@gmail.com>
2022-06-11 23:03:36 +0300

    Add HTTPCookiePropertyKey extension

---
+++ HTTPCookieSync/Sources/Extensions/HTTPCookiePropertyKey+HTTPCookieSync.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPCookiePropertyKey+HTTPCookieSync.swift
+//  HTTPCookieSync
+//
+//  Created by Dmytrii Golovanov on 10.06.2022.
+//  Copyright © 2022 Dmytrii Golovanov. All rights reserved.
+//
+
+import Foundation
+
+extension HTTPCookiePropertyKey {
+    static let created: HTTPCookiePropertyKey = .init("Created")
+}
