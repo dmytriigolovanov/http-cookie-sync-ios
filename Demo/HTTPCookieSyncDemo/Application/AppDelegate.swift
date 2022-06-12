315cf3863c1f765638a9a32cccf9c09a6e037693
Dmytrii Golovanov <dmytrii.golovanov@gmail.com>
2022-06-12 18:52:24 +0300

    Add demo application

---
+++ Demo/HTTPCookieSyncDemo/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  HTTPCookieSyncDemo
+//
+//  Created by Dmytrii Golovanov on 12.06.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Demo/HTTPCookieSyncDemo/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HTTPCookieSyncDemo
+//
+//  Created by Dmytrii Golovanov on 12.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
