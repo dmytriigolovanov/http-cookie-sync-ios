c35d149c635ab8f7cc7b4556d28cac696e8bb798
Dmytrii Golovanov <dmytrii.golovanov@gmail.com>
2022-06-12 19:28:36 +0300

    Add demo AppConfiguration

---
+++ Demo/HTTPCookieSyncDemo/Application/AppConfiguration.swift
@@ -0,0 +1,46 @@
+//
+//  AppConfiguration.swift
+//  HTTPCookieSyncDemo
+//
+//  Created by Dmytrii Golovanov on 12.06.2022.
+//
+
+import Foundation
+import WebKit
+import HTTPCookieSync
+
+struct AppConfiguration {
+    static var urlSession: URLSession = {
+        return URLSession.shared
+    }()
+    
+    static var httpCookieStorage: HTTPCookieStorage = {
+        guard let httpCookieStorage = urlSession.configuration.httpCookieStorage else {
+            fatalError("Provided URLSession doesn't contain HTTPCookieStorage.")
+        }
+        return httpCookieStorage
+    }()
+    
+    // MARK: WebKit
+    
+    static var websiteDataSource: WKWebsiteDataStore = {
+        return WKWebsiteDataStore.default()
+    }()
+    
+    static var webViewConfiguration: WKWebViewConfiguration {
+        let configuration = WKWebViewConfiguration()
+        configuration.websiteDataStore = websiteDataSource
+        return WKWebViewConfiguration()
+    }
+    
+    // MARK: HTTPCookieSync
+    
+    static var httpCookieSyncer: HTTPCookieSyncer = {
+        return HTTPCookieSyncer(
+            storages: [
+                httpCookieStorage,
+                websiteDataSource.httpCookieStore
+            ]
+        )
+    }()
+}
