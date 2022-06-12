f32dfe457cf7e64519af60d0ae5a8bbf064f1e7b
Dmytrii Golovanov <dmytrii.golovanov@gmail.com>
2022-06-12 19:28:59 +0300

    Setup demo webview

--- Demo/HTTPCookieSyncDemo/Presentation/ViewController.swift
+++ Demo/HTTPCookieSyncDemo/Presentation/ViewController.swift
@@ -6,10 +6,56 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    private var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Create WebView
+        
+        webView = WKWebView(
+            frame: CGRect(
+                origin: .zero,
+                size: view.bounds.size
+            ),
+            configuration: AppConfiguration.webViewConfiguration
+        )
+        webView.navigationDelegate = self
+        view.addSubview(webView)
+        
+        // Load URL
+        
+        if let url = URL(string: "https://github.com/dmytriigolovanov/http-cookie-sync-ios") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+}
+
+// MARK: - WKNavigationDelegate
+
+extension ViewController: WKNavigationDelegate {
+    func webView(
+        _ webView: WKWebView,
+        decidePolicyFor navigationAction: WKNavigationAction,
+        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+    ) {
+        AppConfiguration.httpCookieSyncer.sync {
+            decisionHandler(.allow)
+        }
+    }
+    
+    func webView(
+        _ webView: WKWebView,
+        decidePolicyFor navigationResponse: WKNavigationResponse,
+        decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void
+    ) {
+        AppConfiguration.httpCookieSyncer.sync {
+            decisionHandler(.allow)
+        }
     }
 }
